0fa47e35ecc0cd3f122ec2e9ee65be6ccff74764
Ayub Adiputra <ayubna32@gmail.com>
2024-07-08 00:17:59 +0700

    feat(Module, Conversion and Attributes): Add myElement element to Divi4Module attributes. In addition, add conversion attributes for my_element_width sizing properties.

--- src/components/d4-module/conversion-outline.ts
+++ src/components/d4-module/conversion-outline.ts
@@ -54,5 +54,8 @@ export const conversionOutline: ModuleConversionOutline = {
     content:      'content.innerContent.*',
     title:        'title.innerContent.*',
     header_level: 'title.decoration.font.font.*.headingLevel',
+    my_element_width_width:            'myElement.decoration.sizing.*.width',
+    my_element_width_max_width:        'myElement.decoration.sizing.*.maxWidth',
+    my_element_width_module_alignment: 'myElement.decoration.sizing.*.alignment',
   },
 };
